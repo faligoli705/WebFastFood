6ca0dc0309094f4f9b8cb67e28cfc3f36277e593
faligoli705 <f.aligoli70.5@gmail.com>
2021-01-05 14:56:15 +0330

    1399-10-16

--- WebFramwork/Middlewares/CustomExceptionHandlingMiddleware.cs
+++ WebFramwork/Middlewares/CustomExceptionHandlingMiddleware.cs
@@ -8,19 +8,37 @@ using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Common;
+using WebFramwork.Api;
 
 namespace WebFramwork.Middlewares
 {
    public class CustomExceptionHandlingMiddleware
     {
         private readonly RequestDelegate _next;
-        public CustomExceptionHandlingMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-       public async Task  Invoke(HttpContext httpContext)
-        {
+        private readonly ILogger<CustomExceptionHandlingMiddleware> _logger;
 
+        public CustomExceptionHandlingMiddleware(RequestDelegate next,ILogger<CustomExceptionHandlingMiddleware> logger)
+        {
+            this._next = next;
+            this._logger = logger;
+        }
+
+
+        public async Task  Invoke(HttpContext httpContext)
+        {
+            try
+            {
+               await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex,"خطای ناشناخته رخ داده است");
+                var apiResult = new ApiResult(false, ApiResultStatusCode.ServerError);
+                var json =JsonConvert.SerializeObject(apiResult);
+                await httpContext.Response.WriteAsync(json);
+            }
         }
     }
    
